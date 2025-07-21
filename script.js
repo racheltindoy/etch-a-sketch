09130c6a8fe0207496d7d4ace929b96ed6b9f391
Rachel Tindoy <tindoyrachel@gmail.com>
2025-07-22 02:31:27 +0800

    Reorganize code

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const body = document.querySelector('body');
 const linesRow = document.createElement('div');
 const linesColumn = document.createElement('div');
-
+const xGrids = findAllXGridPositions();
+const yGrids = findAllYGridPositions();
 linesRow.setAttribute('id', 'row');
 linesColumn.setAttribute('id', 'column');
-
 body.appendChild(linesRow);
 body.appendChild(linesColumn);
 
+// Create rows
 for(let i = 0; i < 16; i++) {
 	const linesRowGrid = document.createElement('div');
 	linesRow.appendChild(linesRowGrid);
 	linesRowGrid.classList.add('row_grid');
 }
 
+
+// Create columns
 for(let i = 0; i < 16; i++) {
 	const linesColumnGrid = document.createElement('div');
 	linesColumn.appendChild(linesColumnGrid);
 	linesColumnGrid.classList.add('column_grid');
 }
+
+
 
 function findAllXGridPositions() {
 	let xGrids = document.querySelectorAll('.row_grid');
@@ -36,6 +41,7 @@ function findAllXGridPositions() {
 	return xBottomArray;
 }
 
+
 function findAllYGridPositions() {
 	let yGrids = document.querySelectorAll('.column_grid');
 	let yGridsNum = yGrids.length;
@@ -52,73 +58,82 @@ function findAllYGridPositions() {
 	return yRightArray;
 }
 
-const xGrids = findAllXGridPositions();
-const yGrids = findAllYGridPositions();
 
-console.log(xGrids);
-console.log(yGrids);
-
-let x_grid = document.querySelectorAll('.row_grid');
-	const x = x_grid[0].getBoundingClientRect();
-	const xTop = x.bottom;
-	// console.log(xTop);
-
-	let x2_grid = document.querySelectorAll('.row_grid');
-	const x2 = x_grid[1].getBoundingClientRect();
-	const x2Top = x2.top;
-	// console.log(x2Top);
+console.log("X GRIDS: " + xGrids);
+console.log("Y GRIDS: " + yGrids);
 
 
-// let isDrawing = false;
+linesRow.addEventListener('click', (e) => {
+	console.log(xGrids);
+	console.log(yGrids);
 
-// linesRow.addEventListener('mousedown', () => {
-// 	isDrawing = true;
-// })
-
-// linesRow.addEventListener('mouseup', () => {
-// 	isDrawing = false;
-// });
-
-// linesRow.addEventListener('mousemove', (e) => {
-// 	// console.log("X: " + e.clientX);
-// 	// console.log("Y: " + e.clientY);
-// 	if(!isDrawing) {return}
-
-// 	let pixel = document.createElement('div');
-// 	pixel.classList.add('pixel');
-// 	linesRow.appendChild(pixel);
-// 	pixel.setAttribute('style', `left: ${e.clientX}px; top: ${e.clientY}px`);
-
-// });
-
-
-
-
-
-let y_grid = document.querySelectorAll('.column_grid');
-const y = y_grid[0].getBoundingClientRect();
-const yTop = y.right;
-// console.log(yTop);
-
-let y2_grid = document.querySelectorAll('.column_grid');
-const y2 = y_grid[1].getBoundingClientRect();
-const y2Top = y2.left;
-// console.log(y2Top);
-
-
-linesRow.addEventListener('click', () => {
+	const x = e.clientX;
+	const y = e.clientY;
+	const xGridsLength = xGrids.length;
+	const yGridsLength = yGrids.length;
+	let x1;
+	let x2;
+	let y1;
+	let y2;
+	
 	let pixel = document.createElement('div');
-	pixel.setAttribute('style', `width: ${y2Top - yTop}px; height: ${x2Top - xTop}px; position: absolute; top: ${xTop}px; left: ${yTop}px; background: red`);
+
+	for(let i = 0; i <= xGridsLength; i++) {
+		let j;
+		if(i !== xGridsLength) { j = i+1; } else { j = i; }
+
+		if(x >= xGrids[i] && x < xGrids[j]) {
+			x1 = xGrids[i];
+			x2 = xGrids[j];
+			console.log("X1: " + x1);
+			console.log("X2: " + x2);
+			console.log("XGRIDS[i]: " + xGrids[i]);
+			console.log("XGRIDS[j]" + xGrids[j]);
+		}
+	}
+
+	for(let i = 0; i <= yGridsLength; i++) {
+		let j;
+		if(i !== yGridsLength) { j = i+1; } else { j = i; }
+
+		if(y >= yGrids[i] && y < yGrids[j]) {
+			y1 = yGrids[i];
+			y2 = yGrids[j];
+
+			console.log("Y1: " + y1);
+			console.log("Y2: " + y2);
+			console.log("YGRIDS[i]: " + yGrids[i]);
+			console.log("YGRIDS[j]" + yGrids[j]);
+		}
+	}
+
+	let width = y2-y1;
+	let height = x2-x1;
+
+	pixel.setAttribute('style', 
+		`
+		width: ${width}px; 
+		height: ${height}px; 
+		position: absolute; 
+		top: ${x}px; 
+		left: ${y}px; 
+		background: red
+		`
+	);
+
 	linesRow.appendChild(pixel);
 
-	console.log("it's clicked");
+	console.log("---------------------------------------------");
+
+	console.log("x: " + x);
+	console.log("y: " + y);
+	console.log("x1: " + x1);
+	console.log("x2: " + x2);
+	console.log("y1: " + y1);
+	console.log("y2: " + y2);
+	console.log("---------------------------------------------");
+	
 });
-
-
-
-
-// const rect = myDiv.getBoundingClientRect();
-
 
 
 // linesRow.addEventListener('click', (e) => {
@@ -129,3 +144,52 @@ linesRow.addEventListener('click', () => {
 // 	pixel.classList.add('pixel');
 // 	pixel.setAttribute('style', `left: ${e.clientX}px; top: ${e.clientY}px`);
 // });
+
+
+// let x_grid = document.querySelectorAll('.row_grid');
+// 	const x = x_grid[0].getBoundingClientRect();
+// 	const xTop = x.bottom;
+// 	// console.log(xTop);
+
+// 	let x2_grid = document.querySelectorAll('.row_grid');
+// 	const x2 = x_grid[1].getBoundingClientRect();
+// 	const x2Top = x2.top;
+// console.log(x2Top);
+
+
+// let y_grid = document.querySelectorAll('.column_grid');
+// const y = y_grid[0].getBoundingClientRect();
+// const yTop = y.right;
+// console.log(yTop);
+
+// let y2_grid = document.querySelectorAll('.column_grid');
+// const y2 = y_grid[1].getBoundingClientRect();
+// const y2Top = y2.left;
+// console.log(y2Top);
+
+
+
+// draw on mousemove
+// function draw() {
+// 	let isDrawing = false;
+
+// 	linesRow.addEventListener('mousedown', () => {
+// 		isDrawing = true;
+// 	})
+
+// 	linesRow.addEventListener('mouseup', () => {
+// 		isDrawing = false;
+// 	});
+
+// 	linesRow.addEventListener('mousemove', (e) => {
+// 		// console.log("X: " + e.clientX);
+// 		// console.log("Y: " + e.clientY);
+// 		if(!isDrawing) {return}
+
+// 		let pixel = document.createElement('div');
+// 		pixel.classList.add('pixel');
+// 		linesRow.appendChild(pixel);
+// 		pixel.setAttribute('style', `left: ${e.clientX}px; top: ${e.clientY}px`);
+
+// 	});
+// }
